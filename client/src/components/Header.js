23444c2e4d2073e947b8fd5e00b260529d917c92
Chris Kratz <Ckratz17@gmail.com>
2022-09-22 12:59:46 -0400

    initial backend with signup and log in modals

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,11 +9,10 @@ function Header() {
     const [showModal, setShowModal] = useState(false)
     return (
         <div>
-             <Container fluid>
+             {/* <Container fluid>
                 <Navbar.Toggle aria-controls='navbar' />
                 <Navbar.Collapse id='navbar'>
                     <Nav className='ml-auto'>
-                        {/* if user is logged in show saved books and logout */}
                         {Auth.loggedIn() ? (
                             <>
                                 <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
@@ -25,13 +24,12 @@ function Header() {
                 </Navbar.Collapse>
             </Container>
   
-            {/* set modal data up */ }
+         
             <Modal
                 size='lg'
                 show={showModal}
                 onHide={() => setShowModal(false)}
                 aria-labelledby='signup-modal'>
-                {/* tab container to do either signup or login component */}
                 <Tab.Container defaultActiveKey='login'>
                 <Modal.Header closeButton>
                     <Modal.Title id='signup-modal'>
@@ -56,8 +54,8 @@ function Header() {
                     </Tab.Content>
                 </Modal.Body>
                 </Tab.Container>
-            </Modal>
-            <img  className="jumbotron" src={animeBg} alt="header"></img>
+            </Modal> */}
+            <img  className="image" src={animeBg} alt="header"></img>
         </div >
     )
 }
